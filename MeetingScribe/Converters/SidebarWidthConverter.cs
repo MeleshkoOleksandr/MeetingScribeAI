8b964db37f13fa911d977a28ec0f5cbd9ab751a3
Alex Meleshko <4882672@gmail.com>
2026-06-11 16:43:31 +0200

    First UI prototype

---
+++ MeetingScribe/Converters/SidebarWidthConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace MeetingScribe.Converters;
+
+public class SidebarWidthConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool isExpanded)
+        {
+            return isExpanded ? 240.0 : 64.0;
+        }
+        return 240.0;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
