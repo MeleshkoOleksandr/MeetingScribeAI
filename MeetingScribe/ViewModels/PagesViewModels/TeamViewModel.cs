b27460ec0644673964842604b184a1e9f1a5580f
Alex Meleshko <4882672@gmail.com>
2026-07-20 11:16:04 +0200

    Adding command for editing buttons for Participants list

--- MeetingScribe/ViewModels/PagesViewModels/TeamViewModel.cs
+++ MeetingScribe/ViewModels/PagesViewModels/TeamViewModel.cs
@@ -2,10 +2,13 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MeetingScribe.Logic.Meeting;
 using MeetingScribe.Logic.Services;
+using MeetingScribe.UILogic.Enums;
+using MeetingScribe.Views;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace MeetingScribe.ViewModels;
 
@@ -25,8 +28,20 @@ public partial class TeamViewModel : ViewModelBase
 
         //If we adding or removing participants, we want to update the filtered list as well
         Participants.CollectionChanged += (s, e) => OnPropertyChanged(nameof(FilteredParticipants));
+        // If we adding list (Add, Remove, Clear - save changes to disk
+        Participants.CollectionChanged += (s, e) => AutoSaveToDisk();
+        Groups.CollectionChanged += (s, e) => AutoSaveToDisk();
     }
 
+    private void AutoSaveToDisk()
+    {
+        // Wrire to file when participants or groups are changed
+        TeamStorageService.SaveData(Participants, Groups);
+        System.Diagnostics.Debug.WriteLine("Structure auto-saved to disk.");
+    }
+
+    // ------   Filters  ------
+    // ------------------------
     public IEnumerable<Participant> FilteredParticipants
     {
         get
@@ -45,6 +60,8 @@ public partial class TeamViewModel : ViewModelBase
         OnPropertyChanged(nameof(FilteredParticipants));
     }
 
+    // ------   Participant  ------
+    // ----------------------------
     [RelayCommand]
     private void AddParticipant()
     {
@@ -54,8 +71,67 @@ public partial class TeamViewModel : ViewModelBase
     }
 
     [RelayCommand]
-    private void SaveAll() => TeamStorageService.SaveData(Participants, Groups); // TODO Change Participants and Groups separatly
+    private async Task DeleteParticipant()
+    {
+        if (SelectedParticipant == null) return;
 
-    // The logic behind automatic saving when any field is changed
-    partial void OnSelectedParticipantChanged(Participant? value) => SaveAll();
+        var res = await LuminaMessageBox.Show(
+            "Delete Member?",
+            $"Are you sure you want to remove {SelectedParticipant.Name}?",
+            LuminaMessageBoxType.Danger, "Remove");
+
+        if (res == LuminaMessageBox.MessageBoxResult.Confirm)
+        {
+            Participants.Remove(SelectedParticipant);
+            SelectedParticipant = null;
+        }
+    }
+
+    [RelayCommand]
+    private async Task ClearParticipants()
+    {
+        if (Participants.Count == 0) return;
+
+        var res = await LuminaMessageBox.Show(
+            "Clear All?",
+            "This will remove EVERY participant from the database. This cannot be undone.",
+            LuminaMessageBoxType.Danger, "Clear All");
+
+        if (res == LuminaMessageBox.MessageBoxResult.Confirm)
+        {
+            Participants.Clear();
+            SelectedParticipant = null;
+        }
+    }
+
+    [RelayCommand]
+    private async Task SaveAll()
+    {
+        // Saving current state to disk
+        AutoSaveToDisk();
+
+        await LuminaMessageBox.Show(
+            "Saved",
+            "All member details have been updated successfully.",
+            LuminaMessageBoxType.Message);
+    }
+
+    // ------   Groups  ------
+    // -----------------------
+
+    [RelayCommand]
+    private async Task DeleteGroup()
+    {
+        if (SelectedGroup == null) return;
+        var res = await LuminaMessageBox.Show("Delete Group?", $"Delete {SelectedGroup.Name}?", LuminaMessageBoxType.Danger);
+        if (res == LuminaMessageBox.MessageBoxResult.Confirm) Groups.Remove(SelectedGroup);
+    }
+
+    [RelayCommand]
+    private async Task ClearGroups()
+    {
+        var res = await LuminaMessageBox.Show("Clear Groups?", "Delete all groups?", LuminaMessageBoxType.Danger);
+        if (res == LuminaMessageBox.MessageBoxResult.Confirm) Groups.Clear();
+    }
+
 }
